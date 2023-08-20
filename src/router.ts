b885baaa831eab708301a00cd6d1ba2dfa4c4aea
Hiromu OCHIAI <otiai10@gmail.com>
2023-08-20 14:04:33 +0900

    Add path parameter parser and NotFound

--- src/router.ts
+++ src/router.ts
@@ -8,10 +8,10 @@ type ExtractCallback<T> = T extends chrome.events.Event<infer U> ? U : never;
 type HandlerOf<Callback extends (...args: any[]) => any> = (...args: Parameters<Callback>) => (Promise<any | void> | void);
 type Resolver<Callback extends (...args: any[]) => any, U = {}> = (...args: Parameters<Callback>) => Promise<Resolved<U>>;
 
-// type RouteMatcher<H> = {
-//     match(action: string): boolean;
-//     handelr(): H;
-// }
+type RouteMatcher<H, U = any> = {
+    match(action: string): Resolved<U> | undefined;
+    handelr(): H;
+}
 
 const DefaultResolver = (...args) => {
     const alias = ActionKeyAlias.find(a => args[0][a] !== undefined);
@@ -23,25 +23,60 @@ export class Router<T extends chrome.events.Event<any>, U = {}> {
 
     constructor(private resolver: Resolver<ExtractCallback<T>, U> = DefaultResolver) { }
 
-    private routes: { [action: string]: HandlerOf<ExtractCallback<T>> } = {};
+    // private routes: { [action: string]: HandlerOf<ExtractCallback<T>> } = {};
 
-    // private _routes: {
-    //     exact: RouteMatcher<HandlerOf<ExtractCallback<T>>>[],
-    //     regex: RouteMatcher<HandlerOf<ExtractCallback<T>>>[],
-    // }
+    private notfound: HandlerOf<ExtractCallback<T>> = () => { };
+    public onNotFound(callback: HandlerOf<ExtractCallback<T>>) {
+        this.notfound = callback;
+    }
+
+    private routes: {
+        exact: RouteMatcher<HandlerOf<ExtractCallback<T>>>[],
+        regex: RouteMatcher<HandlerOf<ExtractCallback<T>>>[],
+    } = { exact: [], regex: [] };
 
     public on(action: string, callback: HandlerOf<ExtractCallback<T>>) {
-        // name.split("/").filter
-        this.routes[action] = callback;
+        let includesRegex = false;
+        const segments = action.split("/").filter(c => c !== "").map(c => {
+            if (c.startsWith("{") && c.endsWith("}")) {
+                includesRegex = true;
+                const name = c.slice(1, c.length - 1);
+                return `(?<${name}>[^\\\/]+)`;
+            }
+            return c;
+        })
+        if (!includesRegex) {
+            return this.routes.exact.push({
+                match: (a) => a === action ? { [ActionKey]: action } : undefined,
+                handelr: () => callback,
+            });
+        }
+        const str = "^" + "\\\/" + segments.join("\\\/") + "$";
+        const regex = new RegExp(str);
+        return this.routes.regex.push({
+            match: (act) => {
+                const m = act.match(regex);
+                if (m) return { [ActionKey]: action, ...m.groups };
+                return undefined;
+            },
+            handelr: () => callback,
+        });
+    }
+
+    private findHandler(action: string): HandlerOf<ExtractCallback<T>> {
+        const exact = this.routes.exact.find(r => r.match(action));
+        if (exact) return exact.handelr().bind({ route: exact.match(action) });
+        const regex = this.routes.regex.find(r => r.match(action));
+        if (regex) return regex.handelr().bind({ route: regex.match(action) });
+        return this.notfound;
     }
 
     public listener(): ExtractCallback<T> {
         return ((...args: Parameters<ExtractCallback<T>>) => {
             const sendResponse = this.sendResponse(...args);
             this.resolver(...args).then(route => {
-                // TODO: Handle notfound
-                const fun = this.routes[route[ActionKey]].bind({ route });
-                const res = fun(...args);
+                const fn = this.findHandler(route[ActionKey]);
+                const res = fn(...args);
                 if (res instanceof Promise) res.then(sendResponse);
                 else sendResponse(res);
             })
--- tests/spec/router.spec.ts
+++ tests/spec/router.spec.ts
@@ -30,5 +30,34 @@ describe("Router", () => {
             expect(callback).toBeCalled();
             expect(sendResponse).toBeCalled();
         });
+
+        it("should parse {} in action and path parameters", async () => {
+            const r = new Router();
+            const fn = function(this: any, message: any) {
+                return { message: `Hello, ${this.route.name}!` };
+            };
+            const callback = jest.fn().mockName("callback").mockImplementation(fn);
+            const sendResponse = jest.fn().mockName("sendResponse");
+            r.on("/greet/{name}", callback);
+            r.listener()({ action: "/greet/otiai10", name: "otiai10" }, {}, sendResponse);
+            await new Promise(resolve => setTimeout(resolve, 0));
+            expect(callback).toBeCalled();
+            expect(sendResponse).toBeCalled();
+            expect(sendResponse).toBeCalledWith({ message: "Hello, otiai10!" });
+        });
+
+    });
+
+    describe("onNotFound", () => {
+        it("should register a handler for not found", async () => {
+            const r = new Router();
+            const callback = jest.fn().mockName("callback");
+            const sendResponse = jest.fn().mockName("sendResponse");
+            r.onNotFound(callback);
+            r.listener()({ action: "/notfound" }, {}, sendResponse);
+            await new Promise(resolve => setTimeout(resolve, 0));
+            expect(callback).toBeCalled();
+            expect(sendResponse).toBeCalled();
+        });
     });
 });
