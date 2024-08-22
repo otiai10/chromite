17154a1f588477e05628d050c0d5cc25bf4cb3bc
Hiromu OCHIAI <otiai10@gmail.com>
2024-08-22 17:13:20 +0900

    Fix lint

--- src/logger.ts
+++ src/logger.ts
@@ -1,79 +1,85 @@
-
 export enum LogLevel {
-    DEBUG = 0,
-    INFO = 1,
-    WARN = 2,
-    ERROR = 3,
+  DEBUG = 0,
+  INFO = 1,
+  WARN = 2,
+  ERROR = 3,
 }
 
 export class Logger {
-    constructor(
-        public readonly project: string,
-        public level: LogLevel = LogLevel.INFO,
-    ) { }
+  constructor (
+    public readonly project: string,
+    public level: LogLevel = LogLevel.INFO
+  ) { }
 
-    public readonly emoji: {
-        enabled: boolean;
-        dict: Record<LogLevel, string>;
-    } = {
-        enabled: false,
-        dict: {
-            [LogLevel.DEBUG]: '🐞',
-            [LogLevel.INFO]: '💡',
-            [LogLevel.WARN]: '⚠️',
-            [LogLevel.ERROR]: '🚨',
-        }
-    }
-
-    public readonly style: {
-        enabled: boolean;
-        dict: Record<LogLevel, string>;
-     } = {
-        enabled: true,
-        dict: {
-            [LogLevel.DEBUG]: 'color:WHITE; background-color:CORAL; font-weight:BOLD;',
-            [LogLevel.INFO]:  'color:WHITE; background-color:GREY;  font-weight:BOLD;',
-            [LogLevel.WARN]:  'color:BLACK; background-color:GOLD;  font-weight:BOLD;',
-            [LogLevel.ERROR]: 'color:WHITE; background-color:RED;   font-weight:BOLD;',
-        }
+  public readonly emoji: {
+    enabled: boolean
+    dict: Record<LogLevel, string>
+  } = {
+      enabled: false,
+      dict: {
+        [LogLevel.DEBUG]: '🐞',
+        [LogLevel.INFO]: '💡',
+        [LogLevel.WARN]: '⚠️',
+        [LogLevel.ERROR]: '🚨'
+      }
     }
 
-    public setLevel(level: LogLevel): Logger {
-        this.level = level;
-        return this;
-    }
-    public setEmoji(enabled: boolean, dict: Record<LogLevel, string>): Logger {
-        this.emoji.enabled = enabled;
-        Object.keys(LogLevel).map(k => (dict[k] ? (this.emoji.dict[k] = dict[k]) : null));
-        return this;
-    }
-    public setStyle(enabled: boolean, dict: Record<LogLevel, string>): Logger {
-        this.style.enabled = enabled;
-        Object.keys(LogLevel).map(k => (dict[k] ? (this.style.dict[k] = dict[k]) : null));
-        return this;
+  public readonly style: {
+    enabled: boolean
+    dict: Record<LogLevel, string>
+  } = {
+      enabled: true,
+      dict: {
+        [LogLevel.DEBUG]: 'color:WHITE; background-color:CORAL; font-weight:BOLD;',
+        [LogLevel.INFO]: 'color:WHITE; background-color:GREY;  font-weight:BOLD;',
+        [LogLevel.WARN]: 'color:BLACK; background-color:GOLD;  font-weight:BOLD;',
+        [LogLevel.ERROR]: 'color:WHITE; background-color:RED;   font-weight:BOLD;'
+      }
     }
 
-    public debug(...args: unknown[]): void {
-        if (this.level > LogLevel.DEBUG) return;
-        this.print(LogLevel.DEBUG, args);
-    }
-    public info(...args: unknown[]): void {
-        if (this.level > LogLevel.INFO) return;
-        this.print(LogLevel.INFO, args);
-    }
-    public warn(...args: unknown[]): void {
-        if (this.level > LogLevel.WARN) return;
-        this.print(LogLevel.WARN, args);
-    }
-    public error(...args: unknown[]): void {
-        if (this.level > LogLevel.ERROR) return;
-        this.print(LogLevel.ERROR, args);
-    }
+  public setLevel (level: LogLevel): Logger {
+    this.level = level
+    return this
+  }
 
-    private print(level: LogLevel, args: unknown[]): void {
-        const label = LogLevel[level];
-        const head = `(${this.project}) ${this.emoji.enabled ? `${this.emoji.dict[level]} ` : ''}%c[${label}]`;
-        const style = this.style.enabled ? this.style.dict[level] : '';
-        console.log(head, style, ...args);
-    }
+  public setEmoji (enabled: boolean, dict: Record<LogLevel, string>): Logger {
+    this.emoji.enabled = enabled
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+    Object.keys(LogLevel).map(k => (dict[k] ? (this.emoji.dict[k] = dict[k]) : null))
+    return this
+  }
+
+  public setStyle (enabled: boolean, dict: Record<LogLevel, string>): Logger {
+    this.style.enabled = enabled
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+    Object.keys(LogLevel).map(k => (dict[k] ? (this.style.dict[k] = dict[k]) : null))
+    return this
+  }
+
+  public debug (...args: unknown[]): void {
+    if (this.level > LogLevel.DEBUG) return
+    this.print(LogLevel.DEBUG, args)
+  }
+
+  public info (...args: unknown[]): void {
+    if (this.level > LogLevel.INFO) return
+    this.print(LogLevel.INFO, args)
+  }
+
+  public warn (...args: unknown[]): void {
+    if (this.level > LogLevel.WARN) return
+    this.print(LogLevel.WARN, args)
+  }
+
+  public error (...args: unknown[]): void {
+    if (this.level > LogLevel.ERROR) return
+    this.print(LogLevel.ERROR, args)
+  }
+
+  private print (level: LogLevel, args: unknown[]): void {
+    const label = LogLevel[level]
+    const head = `(${this.project}) ${this.emoji.enabled ? `${this.emoji.dict[level]} ` : ''}%c[${label}]`
+    const style = this.style.enabled ? this.style.dict[level] : ''
+    console.log(head, style, ...args)
+  }
 }
--- tests/spec/logger.spec.ts
+++ tests/spec/logger.spec.ts
@@ -1,29 +1,29 @@
-import { Logger, LogLevel } from "../../src/logger";
+import { Logger, LogLevel } from '../../src/logger'
 
-describe("Logger", () => {
-    describe("new", () => {
-        it("should create a logger instance", () => {
-            const logger = new Logger("test");
-            expect(logger).toBeInstanceOf(Logger);
-        });
+describe('Logger', () => {
+  describe('new', () => {
+    it('should create a logger instance', () => {
+      const logger = new Logger('test')
+      expect(logger).toBeInstanceOf(Logger)
     })
-    describe("level", () => {
-        it("should have INFO level by default", () => {
-            console.log = jest.fn();
-            const logger = new Logger("test");
-            expect(logger.level).toBe(1);
-            logger.debug("hello", "world");
-            expect(console.log).not.toBeCalled();
-            logger.setLevel(LogLevel.DEBUG);
-            expect(logger.level).toBe(0);
-            logger.debug("hello", "world", "again");
-            expect(console.log).toBeCalledWith("(test) %c[DEBUG]", "color:WHITE; background-color:CORAL; font-weight:BOLD;", "hello", "world", "again");
-            logger.info("hello", "world", "info");
-            expect(console.log).toBeCalledWith("(test) %c[INFO]", "color:WHITE; background-color:GREY;  font-weight:BOLD;", "hello", "world", "info");
-            logger.warn("hello", "world", "warn");
-            expect(console.log).toBeCalledWith("(test) %c[WARN]", "color:BLACK; background-color:GOLD;  font-weight:BOLD;", "hello", "world", "warn");
-            logger.error("hello", "world", "error");
-            expect(console.log).toBeCalledWith("(test) %c[ERROR]", "color:WHITE; background-color:RED;   font-weight:BOLD;", "hello", "world", "error");
-        });
-    });
-});
+  })
+  describe('level', () => {
+    it('should have INFO level by default', () => {
+      console.log = jest.fn()
+      const logger = new Logger('test')
+      expect(logger.level).toBe(1)
+      logger.debug('hello', 'world')
+      expect(console.log).not.toBeCalled()
+      logger.setLevel(LogLevel.DEBUG)
+      expect(logger.level).toBe(0)
+      logger.debug('hello', 'world', 'again')
+      expect(console.log).toBeCalledWith('(test) %c[DEBUG]', 'color:WHITE; background-color:CORAL; font-weight:BOLD;', 'hello', 'world', 'again')
+      logger.info('hello', 'world', 'info')
+      expect(console.log).toBeCalledWith('(test) %c[INFO]', 'color:WHITE; background-color:GREY;  font-weight:BOLD;', 'hello', 'world', 'info')
+      logger.warn('hello', 'world', 'warn')
+      expect(console.log).toBeCalledWith('(test) %c[WARN]', 'color:BLACK; background-color:GOLD;  font-weight:BOLD;', 'hello', 'world', 'warn')
+      logger.error('hello', 'world', 'error')
+      expect(console.log).toBeCalledWith('(test) %c[ERROR]', 'color:WHITE; background-color:RED;   font-weight:BOLD;', 'hello', 'world', 'error')
+    })
+  })
+})
